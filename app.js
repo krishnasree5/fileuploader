f06163c7800f6911eca082c0c7221fdee58b1b0e
{Krishna <gkrishnasree5@gmail.com>
2025-02-11 15:13:59 +0530

    set urlencoded to true

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(index);
 
 app.listen(port, () => {
