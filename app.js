ef388d4dfcc48015c5baef758a8a874fc98e055f
{Krishna <gkrishnasree5@gmail.com>
2025-06-17 06:49:45 +0530

    for deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import index from "./routes/routes.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -11,4 +11,11 @@ app.use(index);
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
 });
